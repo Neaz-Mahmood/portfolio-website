23201def9a656d0dcacd73f8cd253664c88f2625
Neaz Mahmood <neazn694@gmail.com>
2023-12-06 01:16:53 +0600

    fixed the position of hero image

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -59,13 +59,13 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[500px] lg:h-[500px] relative">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
               src="/images/neaz-m.png"
               alt="hero image"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              width={300}
-              height={300}
+              className="absolute transform -translate-x-1/2 -translate-y-[300px] top-1/2 left-1/2"
+              width={250}
+              height={250}
             />
           </div>
         </motion.div>
